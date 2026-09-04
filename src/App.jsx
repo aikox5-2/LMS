11d08ca8d9e14bf838ba5b17f53d872340efb70c
Aiko X5-2 <ahmadfarhaned@gmail.com>
2026-09-04 09:56:10 +0700

    Part 4

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Button from "./components/button";
+import Input from "./components/input";
+import Modal from "./components/modal";
+
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/*"
+          element={
+            <div className="flex flex-col gap-4 w-96 justify-center p-8 items-center mx-auto min-h-screen">
+              <Input
+                label="name"
+                id="name"
+                placeholder="Masukkan nama anda"
+                required
+              />
+              <Button onClick={() => setIsModalOpen(true)}>Click Me</Button>
+              <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <div className="p-6">
+                  <h2 className="text-lg font-semibold text-[#344054]">
+                    Modal Example
+                  </h2>
+                  <p className="mt-2 text-sm text-[#667085]">
+                    Ini hanya mockup modal sederhana.
+                  </p>
+                  <div className="mt-6 flex justify-end">
+                    <Button onClick={() => setIsModalOpen(false)}>Close</Button>
+                  </div>
+                </div>
+              </Modal>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
